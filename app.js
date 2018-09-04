c572545e5936e5fb21db7f8c84efd3f011136fd6
noah <noahwoodward2244@gmail.com>
2018-09-04 15:01:54 -0700

    Added Basic Routes

--- app.js
+++ app.js
@@ -1,8 +1,22 @@
-const express = require('express')
-const app = express()
+const express = require('express');
+const app = express();
+var exphbs = require('express-handlebars');
+
+app.engine('handlebars', exphbs({defaultLayout: 'main'}));
+app.set('view engine','handlebars');
 
 app.get('/', (req, res) => {
-    res.send('Hello World!')
+    res.render('home',{ msg: 'Hello World!'});
+  })
+
+  // Start to Review section 
+  let reviews = [
+      {title: "Great Review"},
+      {title: "Next Review"}
+  ]
+  //Reviews route 
+  app.get('/reviews', (req, res) => {
+    res.render('reviews-index', { reviews: reviews });
   })
 
   app.listen(3000, () => {

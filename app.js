8ceaecad49989a7028486a08ac4a83e28bdf2340
noah <noahwoodward2244@gmail.com>
2018-09-04 16:15:59 -0700

    Added MongoDB Connection

--- app.js
+++ app.js
@@ -1,26 +1,31 @@
 const express = require('express');
 const app = express();
 var exphbs = require('express-handlebars');
+const mongoose = require('mongoose');
+mongoose.connect('mongodb://localhost/rotten-potatoes', { useMongoClient: true });
+
+const Review = mongoose.model('Review', {
+    title: String
+  });
 
 app.engine('handlebars', exphbs({defaultLayout: 'main'}));
 app.set('view engine','handlebars');
 
-app.get('/', (req, res) => {
-    res.render('home',{ msg: 'Hello World!'});
-  })
-
-  // Start to Review section 
-  let reviews = [
-      {title: "Great Review"},
-      {title: "Next Review"}
-  ]
   //Reviews route 
-  app.get('/reviews', (req, res) => {
-    res.render('reviews-index', { reviews: reviews });
-  })
+  app.get('/', (req, res) => {
+   Review.find()
+    .then(reviews => {
+        res.render('reviews-index', { reviews: reviews});
+    })
+    .catch(err => {
+        console.log(err)
+    })
+  });
+
+
 
   app.listen(3000, () => {
     console.log('App listening on port 3000!')
-  })
+  });
 
  
